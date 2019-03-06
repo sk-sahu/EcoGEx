173df8f580f89bbdf496ae1c25d2e58f6b71894c
Sangram Keshari Sahu <sk-sahu@github.com>
2019-03-07 00:10:24 +0530

    CMD check fix

--- R/runEcoGEx.R
+++ R/runEcoGEx.R
@@ -1,5 +1,11 @@
+#' Running EcoGEx locally
+#'
+#' This function will run the EcoGEx shiny app locally
+#' @return EcoGEx shinny app
+#'
+#' @export
 runEcoGEx <- function() {
- 
+
  appDir <- system.file("app", package = "EcoGEx")
 
   shiny::runApp(appDir, display.mode = "normal")
